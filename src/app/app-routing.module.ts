f7b205aca6bc5999d2cd2ef8ef411f2476e3d161
Saravanan S <saravanans.santhanakrishnan@gmail.com>
2024-09-24 15:45:46 +0530

    removed additional urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/secured/dashboard',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   },
   {
-    path: 'public',  // Root path for public routes
+    path: '',  // Root path for public routes
     loadChildren: () => import('./public/public.module').then(m => m.PublicModule)  // Lazy load PublicModule
   },
   {
-    path: 'secured',  // Path for secured routes
+    path: '',  // Path for secured routes
     loadChildren: () => import('./secured/secured.module').then(m => m.SecuredModule)  // Lazy load SecuredModule
   },
   {
